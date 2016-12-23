b257c9dadf8a68626ce84cf03a55681b02cef4b3
Karen Chobanyan <karen@atero.solutions>
2016-12-23 13:59:53 +0400

    Table loop

--- index.php
+++ index.php
@@ -17,16 +17,12 @@ if (!$db_selected) {
     die('Can\'t use foo : '.mysql_error());
 }
 
-$sql = 'SELECT u.users_id, u.first_name, u.last_name, GROUP_CONCAT(t.name SEPARATOR ",") AS tms
+$sql = 'SELECT u.id, u.first_name, u.last_name, GROUP_CONCAT(t.name SEPARATOR ",") AS tms
 FROM users u
-inner join teams_users tu on tu.user_id=u.users_id
-inner join teams t on t.teams_id=tu.team_id
-GROUP BY u.users_id';
+inner join teams_users tu on tu.user_id=u.id
+inner join teams t on t.id=tu.team_id
+GROUP BY u.id';
 $users = mysqli_query($conn, $sql);
-while ($row = mysqli_fetch_assoc($users)) {
-  print_r($row);
-
-}
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,7 +41,12 @@ while ($row = mysqli_fetch_assoc($users)) {
         </tr>
       </thead>
       <tbody>
-
+        <?php
+while ($row = mysqli_fetch_assoc($users)) {
+    echo '<tr><td>'.$row['id'].'</td>';
+    echo '<td>'.$row['first_name'].' '.$row['first_name'].'</td>';
+    echo '<td>'.$row['tms'].'</td></tr>';
+} ?>
       </tbody>
     </table>
 
